b46547217ef278ee6c4dfaff42496e8cc9020044
Shahzaib Fayyaz <shahzaibdev797@gmail.com>
2026-09-20 11:15:41 +0500

    discover section added

---
+++ components/DiscoverSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function DiscoverSection() {
+  return (
+    <div>
+      <div>
+        <h1>Discover</h1>
+      </div>
+    </div>
+  );
+}
+
+export default DiscoverSection;
